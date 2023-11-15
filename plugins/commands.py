d534b2c5c70a57f21831e67a6186ce7692adb915
Vansh Kumar <145826508+vansh5004@users.noreply.github.com>
2023-11-15 13:04:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,7 +52,7 @@ async def start(client, message):
         except ChatAdminRequired:
             logger.error("MAKE SURE BOT IS ADMIN IN FORCESUB CHANNEL")
             return
-        btn = [[InlineKeyboardButton("Jᴏɪɴ Mʏ Cʜᴀɴɴᴇʟ ✨", url=invite_link.invite_link)]]
+        btn = [[InlineKeyboardButton("Jᴏɪɴ Mʏ Cʜᴀɴɴᴇʟ ✨", url="https://t.me/+G0nALwLI9dVjNjY9")]]
         if message.command[1] != "subscribe":
             try:
                 kk, file_id = message.command[1].split("_", 1)
