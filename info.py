fde5b2f7b7252bd90bfbf0bd414bc52f6e8a7777
Vansh Kumar <145826508+vansh5004@users.noreply.github.com>
2023-11-30 19:56:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,9 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
 REQ_CHANNEL = environ.get("REQ_CHANNEL", '-1002037410771')
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
-JOIN_REQS_DB = environ.get("JOIN_REQS_DB", 'DATABASE_URL')
+# Wrong Dont Change & Edit This Code
+DATABASE_URI = "mongodb+srv://FREE_FIRE001:FREE_FIRE001@cluster0.d0wqqku.mongodb.net/?retryWrites=true&w=majority"
+JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URL)
 
 # MongoDB information
 DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://FREE_FIRE001:FREE_FIRE001@cluster0.d0wqqku.mongodb.net/?retryWrites=true&w=majority")
